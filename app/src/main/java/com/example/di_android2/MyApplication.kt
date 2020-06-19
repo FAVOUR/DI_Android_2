95d307bd09ec73370fbf333b8a6b92826d61e505
FAVOUR <truelyfavoured@gmail.com>
2020-06-19 23:15:07 +0100

    Added an instance of the UserManager in the MyApplication.kt

--- app/src/main/java/com/example/di_android2/MyApplication.kt
+++ app/src/main/java/com/example/di_android2/MyApplication.kt
@@ -1,8 +1,14 @@
 package com.example.di_android2
 
 import android.app.Application
+import com.example.di_android2.storage.SharedPreference
+import com.example.di_android2.user.UserManager
 
 class MyApplication :Application(){
+
+   val userManager by lazy {
+          UserManager(SharedPreference(applicationContext))
+   }
 
 
 }
