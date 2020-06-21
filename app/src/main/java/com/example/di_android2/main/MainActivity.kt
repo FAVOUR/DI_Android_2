e1c58e261cb274c967d98d6cb379d00931cc1473
FAVOUR <truelyfavoured@gmail.com>
2020-06-21 22:28:05 +0100

    Fixing a bug

--- app/src/main/java/com/example/di_android2/main/MainActivity.kt
+++ app/src/main/java/com/example/di_android2/main/MainActivity.kt
@@ -3,10 +3,12 @@ package com.example.di_android2.main
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import com.example.di_android2.MyApplication
 import com.example.di_android2.R
+import com.google.gson.Gson
 
 
 private lateinit var mainViewModel: MainViewModel
@@ -19,10 +21,12 @@ private lateinit var mainViewModel: MainViewModel
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
         val userManager = (application as MyApplication).userManager
 
+        setContentView(R.layout.activity_main)
+
+
+         Log.i("userManager", Gson().toJson(userManager.userDataRepository))
         mainViewModel = MainViewModel(userManager.userDataRepository!!)
         setupViews()
     }
--- app/src/main/java/com/example/di_android2/main/MainViewModel.kt
+++ app/src/main/java/com/example/di_android2/main/MainViewModel.kt
@@ -1,12 +1,18 @@
 package com.example.di_android2.main
 
+import android.util.Log
 import com.example.di_android2.user.UserDataRepository
+import com.google.gson.Gson
 
 class MainViewModel(private val userDataRepository: UserDataRepository) {
 
+init {
+    Log.i("userManager", Gson().toJson(userDataRepository))
 
+}
     val welcomeText: String
         get() = "Hello ${userDataRepository.username}!"
+
 
     val notificationsText: String
         get() = "You have ${userDataRepository.unreadNotifications} unread notifications"
