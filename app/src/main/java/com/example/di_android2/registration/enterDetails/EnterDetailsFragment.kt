cb574800f4c88d63521413c0bf04cb47476008fe
FAVOUR <truelyfavoured@gmail.com>
2020-06-22 10:55:52 +0100

    Corrected the spelling from EnterDeteilsFragment.kt to EnterDetailsFragment.kt

--- app/src/main/java/com/example/di_android2/registration/enterDetails/EnterDetailsFragment.kt
+++ app/src/main/java/com/example/di_android2/registration/enterDetails/EnterDetailsFragment.kt
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [EnterDeteilsFragment.newInstance] factory method to
+ * Use the [EnterDetailsFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class EnterDeteilsFragment : Fragment() {
+class EnterDetailsFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
