fe9b9f21c6e82b3ca4a3e5991c4d9aa846a5180b
FAVOUR <truelyfavoured@gmail.com>
2020-06-28 11:18:07 +0100

    Done with SettingsActivity.kt

--- app/src/main/java/com/example/di_android2/settings/SettingViewmodel.kt
+++ app/src/main/java/com/example/di_android2/settings/SettingViewmodel.kt
@@ -3,7 +3,7 @@ package com.example.di_android2.settings
 import com.example.di_android2.user.UserDataRepository
 import com.example.di_android2.user.UserManager
 
-class SettingViewmodel (val userManager: UserManager,val userDataRepository: UserDataRepository) {
+class SettingViewmodel (val userDataRepository: UserDataRepository,val userManager: UserManager) {
 
     fun refreshNotification(){
         userDataRepository.refreshUnreadNotifications()
--- app/src/main/java/com/example/di_android2/settings/SettingsActivity.kt
+++ app/src/main/java/com/example/di_android2/settings/SettingsActivity.kt
@@ -1,7 +1,9 @@
 package com.example.di_android2.settings
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.di_android2.MyApplication
 import com.example.di_android2.R
 
@@ -16,5 +18,22 @@ class SettingsActivity : AppCompatActivity() {
 
         val userManager=(application as MyApplication).userManager
 
+        settingsViewmodel = SettingViewmodel(userManager.userDataRepository!!,userManager)
+         setupViews()
+    }
+
+    private fun setupViews(){
+        findViewById<Button>(R.id.refresh).setOnClickListener {
+            settingsViewmodel.refreshNotification()
+        }
+
+        findViewById<Button>(R.id.logout).setOnClickListener {
+            settingsViewmodel.logout()
+            val intent =Intent(this,LoginActivity::class.java)
+              intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or
+                              Intent.FLAG_ACTIVITY_CLEAR_TASK or
+                              Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(intent)
+        }
     }
 }
