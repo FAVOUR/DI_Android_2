0458c9d99434eba3c44db0da1e8456b824955dd8
FAVOUR <truelyfavoured@gmail.com>
2020-06-29 08:38:15 +0100

    Write test  for settings viewmodel in the SettingViewmodelTest.kt class

---
+++ app/src/test/java/com/example/di_android2/settings/SettingViewmodelTest.kt
@@ -0,0 +1,48 @@
+package com.example.di_android2.settings
+
+import androidx.lifecycle.ViewModel
+import com.example.di_android2.user.UserDataRepository
+import com.example.di_android2.user.UserManager
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.core.Is.`is`
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mockito.*
+
+class SettingViewmodelTest{
+
+    lateinit var userManager: UserManager
+
+    lateinit var userDataRepository: UserDataRepository
+    lateinit var viewmodel: SettingViewmodel
+
+
+    @Before
+    fun setup(){
+        userDataRepository = mock(UserDataRepository::class.java)
+        userManager = mock(UserManager::class.java)
+
+        viewmodel= SettingViewmodel(userDataRepository,userManager)
+    }
+
+    @Test
+    fun `refresh Notification Works As Expected`(){
+//       `when`(viewmodel.userDataRepository.unreadNotifications).thenReturn(3)
+        viewmodel.refreshNotification()
+//
+        verify(viewmodel).refreshNotification()
+//        assertThat(viewmodel.userDataRepository.unreadNotifications,`is`( equalTo(19)))
+
+
+    }
+
+
+    @Test
+    fun `Logout works as expected`() {
+        viewmodel.logout()
+
+        verify(userManager).logout()
+    }
+
+}
