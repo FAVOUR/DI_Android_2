cf3280baa78e2f998253e1f66babf9720a6da5e3
FAVOUR <truelyfavoured@gmail.com>
2020-06-30 21:52:08 +0100

    Implemented TermsAndConditionsFragment.kt

--- app/src/main/java/com/example/di_android2/registration/termsandcondition/TermsAndConditionsFragment.kt
+++ app/src/main/java/com/example/di_android2/registration/termsandcondition/TermsAndConditionsFragment.kt
@@ -5,21 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.example.di_android2.R
+import com.example.di_android2.registration.RegistrationActivity
+import com.example.di_android2.registration.RegistrationViewModel
 
 class TermsAndConditionsFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    private lateinit var registrationViewModel: RegistrationViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_terms_and_conditions, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_terms_and_conditions, container, false)
+
+        registrationViewModel = (activity as RegistrationActivity).registrationViewModel
+
+        view.findViewById<Button>(R.id.next).setOnClickListener {
+            registrationViewModel.acceptTC()
+            (activity as RegistrationActivity).onTermsAndConditionAccepted()
+        }
+
+        return view    }
 
 
 }
