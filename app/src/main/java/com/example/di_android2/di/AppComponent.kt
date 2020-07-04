cd9fb4233b22c823a77f47ad7e659948bebc83ab
FAVOUR <truelyfavoured@gmail.com>
2020-07-04 09:55:26 +0100

    Added  AppSubComponent.kt class  as a module to AppComponent.kt class.

--- app/src/main/java/com/example/di_android2/di/AppComponent.kt
+++ app/src/main/java/com/example/di_android2/di/AppComponent.kt
@@ -2,13 +2,14 @@ package com.example.di_android2.di
 
 import android.content.Context
 import com.example.di_android2.di.subcomponents.registration_sub_component.RegistrationSubComponent
+import com.example.di_android2.di.subcomponents.registration_sub_component.module.AppSubComponent
 import com.example.di_android2.main.MainActivity
 import dagger.BindsInstance
 import dagger.Component
 import javax.inject.Singleton
 
 @Singleton
-@Component(modules = [StorageModule::class])
+@Component(modules = [StorageModule::class,AppSubComponent::class])
 interface AppComponent {
 
   //classes that can be injected by this component
