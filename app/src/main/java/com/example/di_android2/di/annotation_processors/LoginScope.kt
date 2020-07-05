02c15028caf678fad1293e9ce2407e0fa8ac9501
FAVOUR <truelyfavoured@gmail.com>
2020-07-05 16:21:39 +0100

    Create the LoginScope.kt for annotating  the user manager Class

---
+++ app/src/main/java/com/example/di_android2/di/annotation_processors/LoginScope.kt
@@ -0,0 +1,10 @@
+package com.example.di_android2.di.annotation_processors
+
+import javax.inject.Scope
+
+
+@Scope
+@MustBeDocumented
+@Retention(value = AnnotationRetention.RUNTIME)
+  annotation  class LoginScope {
+}
